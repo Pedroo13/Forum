e8c9624fb0a240bd0450409662c42c4a69103863
Pedro Henrique De Lima <79190116+Pedroo13@users.noreply.github.com>
2021-07-20 14:25:03 -0300

    Add files via upload

---
+++ forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,50 @@
+package br.com.alura.forum.repository;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest//Essa é uma anotação para quando você quer criar uma classe 
+		   //de teste, só que essa classe vai testar um Repository.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository repository;
+	
+	@Autowired
+	private TestEntityManager em;
+	
+	@Test
+	public void DeveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+		String nomeCurso = "HTML 5";
+		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
+		
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+	
+	@Test
+	public void NaoDeveriaCarregarUmCursoCujoNomeNaoEstejaJaCadastrado() {
+		String nomeCurso = "JPA";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNull(curso);
+	}
+
+
+}
